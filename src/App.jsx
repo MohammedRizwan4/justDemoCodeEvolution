2e57f575713e8ae7278b5a69c6f5f3742e05e1c8
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-07-10 10:42:19 +0530

    class based comp, state example

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Greet from './components/Greet.jsx'
-import Props from './components/Props.jsx'
-import ClassBased from './components/ClassBased.jsx'
+import Message from './components/Message.jsx'
 
 function App() {
 
   return (
     <>
-      <ClassBased name="rizwan" address="ahmedabad"/>
-      <ClassBased name="raiyan" address="mumbai"/>
-      <ClassBased name="usman" address="vadodara"/>
+      <Message />
     </>
   )
 }
