d53996e3431394531de635ec054c95fc2d6533a0
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-07-10 10:27:20 +0530

    feat: Add Props component to App
    The Props component is added to the App component to pass and display name and address props.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Greet from './components/Greet.jsx'
+import Props from './components/Props.jsx'
 
 function App() {
 
   return (
     <>
       <h1>Hello world</h1>
-      <Greet />
+      <Props name="rizwan" address="ahmedabad"/>
+      <Props name="raiyan" address="mumbai"/>
+      <Props name="usman" address="vadodara"/>
+
+      <Props name="rizwan" address="ahmedabad">
+        <p>Hello i am children</p>
+      </Props>
+      <Props name="raiyan" address="mumbai">
+        <button>Action</button>
+      </Props>
     </>
   )
 }
