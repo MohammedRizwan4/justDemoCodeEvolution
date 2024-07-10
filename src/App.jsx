0355343a1ebcf67bcbd6ac05ce13443c9c092fb8
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-07-11 03:47:59 +0530

    ref to class based component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import './components/style.css'
-// import RefDemos_Method1 from './components/RefDemos_Method1.jsx'
-import RefDemos_Method2 from './components/RefDemos_Method2.jsx'
+import FocusInput from './components/FocusInput'
 
 function App() {
 
   return (
     <>
-      {/* <RefDemos_Method1 /> */}
-      <RefDemos_Method2 />
+      <FocusInput />
     </>
   )
 }
