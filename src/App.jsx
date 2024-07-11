355a1bc828d1b4a7d199c3c570b29bb3980b629b
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-07-11 19:19:13 +0530

    React useState hook in depth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import UseStateHook from './components/UseStateHook'
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <UseStateHook />
     </>
   )
 }
