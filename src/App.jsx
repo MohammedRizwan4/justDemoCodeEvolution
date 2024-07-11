7b5f504b3fa7a8389383e7b4a929565512c70b1f
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-07-11 23:05:31 +0530

    deleted components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import React, { useState } from 'react'
 import './App.css'
-import ComponentD from './components/ComponentD'
-
-// here we have to create individual context for every value to access
-export const UserContext = React.createContext()
-export const AdminContext = React.createContext()
 
 function App() {
 
 
   return (
     <>
-      <UserContext.Provider value="Rizwan">
-        <AdminContext.Provider value="Admin">
-          <ComponentD />
-        </AdminContext.Provider>
-      </UserContext.Provider>
+      <h1>Hello world</h1>
     </>
   )
 }
