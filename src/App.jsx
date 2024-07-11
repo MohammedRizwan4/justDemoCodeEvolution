00889c6f91a3b7d2797ec8aa9f3cae1d046bb47a
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-07-11 18:22:50 +0530

    deleted old

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-import PostListGet from './components/PostListGet'
-import PostListPost from './components/PostListPost'
 
 function App() {
   return (
     <>
-      <PostListPost />
+      <h1>Hello world</h1>
     </>
   )
 }
