ec3b1151f43721733541db077223e50a0e7018a5
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-07-11 15:25:31 +0530

    deleted old components, now Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import './components/style.css'
-import Counter from './component/Counter'
-import ClickCounterTwo from './component/ClickCounterTwo'
-import HoverCounterTwo from './component/HoverCounterTwo'
 
 function App() {
 
   return (
     <>
-      <Counter>
-        {(count, incrementCount) => (<ClickCounterTwo count={count} incrementCount={incrementCount} />)}
-      </Counter>
-      <Counter>
-        {(count, incrementCount) => (<HoverCounterTwo count={count} incrementCount={incrementCount} />)}
-      </Counter>
+      <h1>Hello world</h1>
     </>
   )
 }
--- src/components/List.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function List({person}) {
-    return (
-        <div>
-            <h1>My name is {person.name}</h1>
-        </div>
-    )
-}
