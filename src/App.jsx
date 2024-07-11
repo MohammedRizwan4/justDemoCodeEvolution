dfdcd4c530ed5eb3c6b366cb34d067a8d38f9b62
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-07-11 17:06:07 +0530

    deleted old component, starting React HTTP

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-import ComponentA from './components/ComponentA'
-import { UserProvider } from './components/userContext'
 
 function App() {
-
-  const info = {
-    name: "Rizwan",
-    age: 21
-  }
-
   return (
     <>
-      {/* <UserProvider value={info}> */}
-        <ComponentA />
-      {/* </UserProvider> */}
+      <h1>Hello world</h1>
     </>
   )
 }
