069604dc7f42654916b2e8789deeb49f61860b98
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-07-11 22:25:39 +0530

    useEffect using button fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-import DataFetching from './components/DataFetching'
 import DataFetchingSingle from './components/DataFetchingSingle'
 
 function App() {
   return (
     <>
-      {/* <DataFetching/> */}
       <DataFetchingSingle />
     </>
   )
--- src/components/DataFetchingSingle.jsx
+++ src/components/DataFetchingSingle.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 const DataFetchingSingle = () => {
     const [post, setPost] = useState({})
     const [id, setId] = useState(1)
+    const [onButtonClick, setOnButtonClick] = useState(1)
 
     useEffect(() => {
         axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -11,15 +12,12 @@ const DataFetchingSingle = () => {
                 setPost(res.data)
             })
             .catch(err => console.log(err))
-    }, [id])
-
-    useEffect(() => {
-        console.log(post)
-    }, [post])
+    }, [onButtonClick])
 
     return (
         <div>
             <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+            <button onClick={() => setOnButtonClick(id)}>Click</button>
             {
                 post.title ? post.title : 'Title is undefined'
             }
