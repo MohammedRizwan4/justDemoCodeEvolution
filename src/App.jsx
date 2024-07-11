5531d3f70e7dd4f1b9b27ad8a7e0880854a4927b
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-07-12 00:56:13 +0530

    useReducer pass action and state as object

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import './App.css'
-import CounterOne from './components/CounterOne'
+import CounterTwo from './components/CounterTwo'
 
 function App() {
 
   return (
     <>
-      <CounterOne />
+      <CounterTwo />
     </>
   )
 }
