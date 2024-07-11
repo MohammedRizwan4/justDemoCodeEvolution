f88f33237b24824a061dbcf9a37e14c5fa4a3025
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2024-07-11 15:17:36 +0530

    render props , similar to HOC

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import './components/style.css'
-import ClickCounter from './component/ClickCounter'
-import HoverCounter from './component/HoverCounter'
+import Counter from './component/Counter'
+import ClickCounterTwo from './component/ClickCounterTwo'
+import HoverCounterTwo from './component/HoverCounterTwo'
 
 function App() {
 
   return (
     <>
-      <ClickCounter name='Rizwan'/>
-      <HoverCounter name='Raiyan'/>
+      <Counter render={(count, incrementCount) => <ClickCounterTwo count={count} incrementCount={incrementCount} />} />
+      <Counter render={(count, incrementCount) => <HoverCounterTwo count={count} incrementCount={incrementCount} />} />
     </>
   )
 }
